02f450fc2f3b45f6f365ebe131029935d43ddea6
ENino713 <enino7@stu.southtexascollege.edu>
2023-02-07 15:47:30 -0600

    Corrections

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,16 @@
 <script setup>
-import MainSearch from './'MainSearch.vue'
+import MainSearch from './MainSearch.vue'
 </script>
 
 <template>
-  <div class="wrapper">
-    <MainSearch />
-
-    <h1>Hello There</h1>
-  </div>
+    <div class="wrapper">
+        <MainSearch />
+        <h1>Hello There</h1>
+    </div>
 </template>
 
 <style scoped lang="postcss">
 .wrapper {
-  @apply container mx-auto flex flex-col gap-4 py-4;
+    @apply container mx-auto flex flex-col gap-4 py-4;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,27 +17,28 @@ const brand = ref(' Company Directory')
     </div>
   </nav>
 </template>
-
 <style scoped lang="postcss">
 nav {
   @apply flex h-20 bg-slate-900 text-slate-200;
+
   .wrapper {
     @apply container mx-auto flex w-full items-center justify-between;
-  .brand {
-    @apply
-    &-title {
-      &-tile {
+
+    .brand {
+      &-title {
         @apply text-2xl font-bold text-yellow-500;
       }
     }
+
     .menu {
       @apply flex gap-2;
-      &-item {
-        @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
 
+      &-item {
+        @apply rounded-md bg-yellow-500 px-4 py-2 hover: text-slate-900;
       }
+
       &-login {
-      @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+        @apply rounded-md bg-red-500 px-4 py-2 text-red-100;
       }
     }
   }
